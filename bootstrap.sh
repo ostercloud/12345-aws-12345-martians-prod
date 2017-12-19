cee94e70bbd6e50c00beddff1557aa892730e600
Allen Oster <osterallen@gmail.com>
2017-12-19 09:23:05 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sleep 120;
-apt-get update; 
-apt-get install -y docker; 
+apt-get update -y; 
+apt-get install -y docker.io; 
 ufw disable;
 docker run -dit --name my-apache-app -p 80:80 httpd
